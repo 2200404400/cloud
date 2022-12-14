b87d9348675dbf8c3dbe61d4ca491321a723d0b6
220044400 <220044400@qq.com>
2022-12-15 00:35:31 +0800

    添加Eureka模块

--- project/src/main/java/org/cloud/project/ProjectApplication.java
+++ project/src/main/java/org/cloud/project/ProjectApplication.java
@@ -3,10 +3,12 @@ package org.cloud.project;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 @EnableOpenApi
+@EnableEurekaClient
 @MapperScan(basePackages = "org.cloud.project.mapper")
 public class ProjectApplication {
 
